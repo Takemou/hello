310a98989dc7ae2c1c55f307062a3185bcbd28f1
Takemou <60023035+Takemou@users.noreply.github.com>
2020-01-18 22:47:01 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 # -*- coting:utf-8 -*-
 #練習１
-list_lang=[]
+list_language=[]
 for i in range(1,6):
-    list_lang.append("({})Hello,Git".format(i))
+    list_language.append("({})Hello,Git".format(i))
 else:
-    print(list_lang)
+    print(list_language)
 
 #練習２
-list_lang=[]
-list_lang.append("Hello,Git")
-print(list_lang)
+list_language=[]
+list_language.append("Hello,Git")
+print(list_language)
