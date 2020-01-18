070080750519225e42dde763ad8dbf9206e823e1
Takemou <60023035+Takemou@users.noreply.github.com>
2020-01-18 18:28:22 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+#練習１
+list_lang=[]
+for i in range(1,6):
+    list_lang.append("({})Hello,Git".format(i))
+else:
+    print(list_lang)
+
+#練習２
+list_lang=[]
+list_lang.append("Hello,Git")
+print(list_lang)
